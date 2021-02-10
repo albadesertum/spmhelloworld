3468c8254316e68c5e001acf6252f3bff35a16ae
Vladimir Psyukalov <vpsyukalov@zuzex.com>
2021-02-10 17:15:57 +0300

    first init

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MyLibrary",
+    products: [
+        .library(name: "MyLibrary", targets: ["MyLibrary"])
+    ],
+    dependencies: [
+        
+    ], targets: [
+        .target(name: "MyLibrary", path: "Sources")
+    ]
+)
