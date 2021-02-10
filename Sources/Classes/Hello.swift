f8d433b34799f4a2b9182d14796f4b38762921a4
Vladimir Psyukalov <vpsyukalov@zuzex.com>
2021-02-10 17:51:01 +0300

    Update6

--- Sources/Classes/Hello.swift
+++ Sources/Classes/Hello.swift
@@ -1,6 +1,10 @@
 public struct Hello {
     public var text = "Hello, World!"
     
+    public static var x: String {
+        return "its x"
+    }
+    
     public init(text: String) {
         self.text = text
     }
